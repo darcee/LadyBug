e1642acf77b7be9eae0d4d3ff6f4202d200e0fc5
Darcee Thomason <darcee.thomason@gmail.com>
2020-05-23 12:41:53 -0700

    Create getreport.js

---
+++ ajax/getreport.js
@@ -0,0 +1,23 @@
+       <script>
+function getReport(reportFile, viewID) {
+  if (reportFile == "") {
+        document.getElementById(viewID).innerHTML = "";
+        return;
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById(viewID).innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", reportFile, true);
+        xmlhttp.send();
+    }
+}
+</script>
